3bb6ba17bd7859a7e53fa90d7cd582f1b2043f9b
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-15 00:02:02 -0800

    refactors code, mongoDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // --------------------------------------------------
 
 const app = express();
@@ -8,54 +9,18 @@ app.use(express.json());
 
 require('dotenv').config();
 
-const homes = [
-    {
-        id: 1,
-        type: 'Apartment',
-        description: 'Well furnished apartment'
-    },
-    {
-        id: 2,
-        type: 'Flat',
-        description: 'Well furnished flat at flatville'
-    }
-]
-
-app.get('/', (req, res) => {
-    res.send('Welcome to express')
-});
-// --------------------------------------------------
-app.get('/api/listing', (req, res) => {
-    res.send(homes)
-});
-// --------------------------------------------------
-app.get('/api/listing/:id', (req, res) => {
-    const home = homes.find(home => home.id === parseInt(req.params.id));
-
-    if(!home) {
-        res.status(400).send('Cannot find a home with that ID.')
-    }
-    res.send(home);
-})
-// --------------------------------------------------
-app.post('/api/listing', (req, res) => {
-
-    if(!req.body.type || !req.body.description) {
-        return res.status(400).send('Title and Description is required')
-    }
-
-    const home = {
-        id: homes.length + 1,
-        type: req.body.type,
-        description: req.body.description,
-    }
-
-    homes.push(home);
-    res.send(home);
-})
-// --------------------------------------------------
-
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+mongoose.connect(process.env.DB_URL, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true 
+}).then(result => {
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+})
+.catch(err => console.log(err));
+
+
+
+
